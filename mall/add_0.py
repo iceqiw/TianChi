d8cbe5803b9ae883a4e23f0ad60105e2b7778755
iceqiw <qqwei1123@163.com>
2017-10-23 11:43:48 +0800

    aaa

--- mall/add_0.py
+++ mall/add_0.py
@@ -4,7 +4,7 @@ from datetime import *
 def splitDate(mallid):
     userMallSet=pd.read_csv('userMallSet.csv')
     userMallSet_m=userMallSet[userMallSet.mall_id==mallid]
-    userMallSet_m['hour']=userMallSet_m['time_stamp'].map(lambda x:datetime.strptime(x,"%Y-%m-%d %H:%M").hour)
+    # userMallSet_m['hour']=userMallSet_m['time_stamp'].apply(lambda x:datetime.strptime(x,"%Y-%m-%d %H:%M").hour)
     userMallSet_m.to_csv('data/userMallSet_'+mallid+'_origin.csv', index=False)
 
 mall=pd.read_csv('mall.csv')
